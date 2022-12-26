c6a399d7b42191a438381d1bd0a5f325c9789197
wangqun <2971749820@qq.com>
2022-12-26 22:59:57 +0800

    init

---
+++ src/views/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <div><el-button type="primary">主要按钮</el-button></div>
+  <el-icon><Aim /></el-icon>
+
+</template>
+
+<script>
+const ENV = import.meta.env
+export default {
+    name: 'Index',
+    setup() {
+        console.log('ENV', ENV)
+    }
+}
+</script>
